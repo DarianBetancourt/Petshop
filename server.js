92a55f0aa0e7657bb526c3be141f70b4dbd11e9a
DarianBetancourt <darianbetancourt8@gmail.com>
2021-09-22 21:08:33 -0400

    Create Server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const userRoutes = require("./routes/user")
+const port = 3000
+
+mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@sandbox.ltkid.mongodb.net/petshop?retryWrites=true&w=majority', {useNewUrlParser:true, useUnifiedTopology: true,})
+
+app.use(express.urlencoded({ extended: false}));
+
+
+app.get('/',(req, res) => {
+    res.send(`<h1> Hello world</h1>I am a node server with express, I will be an API for a PetShop <hr><br>`)
+});
+
+
+
+app.listen(port, () => {
+    console.log('Rodando o servidor na porta '+port)
+});
