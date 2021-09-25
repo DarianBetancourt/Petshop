9074801fd8e8295cf02a098ed56b2a07a963a02e
DarianBetancourt <darianbetancourt8@gmail.com>
2021-09-25 08:23:41 -0400

    testing route protection with authentication using jwt

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,88 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../models/user")
+const jwt = require("../auth/jwt")
+const middle = require("../auth/authMiddleware") //middleware
+
+
+/* const authMiddleware = async (req, res, next) => {
+  const [, token] = req.headers.authorization.split(' ')
+    
+  try {
+    const payload = await jwt.verify(token)
+    console.log ("payload = "+payload)
+    const user = await User.findById(payload.user)
+
+    if (!user) {
+      return res.send(401)
+    }
+
+    req.auth = user
+
+    next()
+  } catch (error) {
+    res.status(401).send(error)
+  }
+} */
+
+router.post("/signup",async(req,res)=>{
+    try {
+        
+        const result = await User.create(req.body)
+        const { password, ...user } = result.toObject()
+        
+        const token = jwt.sign({ user: user.id })
+        
+        res.send({ user, token })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+ 
+})
+
+router.get('/login', async (req, res) => {
+    
+  const [, hash] = req.headers.authorization.split(' ')
+  
+  const [email, password] = Buffer.from(hash, 'base64')
+    .toString()
+    .split(':')
+
+  try {
+    const user = await User.findOne({email}).exec();
+    
+    
+    if (!user) {
+      
+      return res.status(401).send("user not found")
+
+    }else{
+        if(user.isValidPassword(password)){
+            
+            const token = jwt.sign({ user: user.id })
+            res.send({ user, token })
+        
+        }
+       
+        else{
+
+           return res.status(401).send("password error") 
+        
+        }
+
+    }
+
+    
+  } catch (error) {
+    console.log(error)  
+    res.send(error)
+  }
+})
+
 
 router
     .route("/")
-        .get( async (req , res ) => {
+        .get(middle.authMiddleware, async (req , res ) => {
             const users = await User.find({},{password:0}).sort({createAt : 'desc'})
             res.json(users) 
         })
@@ -19,7 +97,6 @@ router
         .get( async( req , res ) => {
             const id = req.params.id
             const user = await User.findById(id,{password:0}) 
-            /* const user = await User.findById(id) */
             res.send([
                 {
                     message : "getting user whit id "+id,
@@ -28,12 +105,12 @@ router
             ])
         })
         .put( async( req , res , next) => {
-            //const message = "updating user whit id "+req.params.id
             req.user = await User.findById(req.params.id)
             next()
         })
         .delete( async( req, res ) => {
-            res.send("deleting user whit id "+req.params.id)
+            await User.findByIdAndDelete(req.params.id);
+            res.send("User whit id:"+req.params.id+" deleted" )
         })
 
 function save_edit(path) {
